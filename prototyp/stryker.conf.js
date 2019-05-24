121efc920040c9ceb24011680244577168ea99f2
Martin Welcker <Martinwelcker@gmx.de>
2019-05-24 21:16:58 +0200

    Initial commit

---
+++ prototyp/stryker.conf.js
@@ -0,0 +1,19 @@
+module.exports = function (config) {
+    config.set(
+        {
+            "mutator": "javascript",
+            "packageManager": "npm",
+            "mutate": ["test.js"],
+            "files": ["test.js"],
+            "reporters": [
+                "html",
+                "clear-text",
+                "progress",
+            ],
+            "testRunner": "mocha",
+            "transpilers": [],
+            "testFramework": "mocha",
+            "coverageAnalysis": "perTest"
+        }
+    );
+}
---
+++ prototyp/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function () {
+    describe('#sample1()', function () {
+        it('sample', function () {
+            assert.equal(true, 1 >= 1);
+        });
+    });
+});
