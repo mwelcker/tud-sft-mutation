a075f39fb9d1b5e0ebbd14b3931cbeefe9b891a4
Alexander Lichter <manniL@gmx.net>
2019-05-29 15:49:53 +0200

    test: failing tests for age check

--- tests/unit/PageAccess.spec.js
+++ tests/unit/PageAccess.spec.js
@@ -1,11 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
 import PageAccess from '@/components/PageAccess.vue'
 import { dateNotSetMessage } from '@/constants'
+import { accessGrantedMessage, notOlderThanEighteenMessage } from '../../src/constants'
 
 describe('PageAccess.vue', () => {
   it('shows error message with unset date', () => {
     const wrapper = shallowMount(PageAccess, {})
     wrapper.find('button').trigger('click')
     expect(wrapper.vm.$data.msg).toBe(dateNotSetMessage)
+  })
+  it('shows access granted message when user is older than 18', () => {
+    const wrapper = shallowMount(PageAccess, {
+      propsData: {
+        date: new Date(1980, 5, 5)
+      }
+    })
+    wrapper.find('button').trigger('click')
+    expect(wrapper.vm.$data.msg).toBe(accessGrantedMessage)
+  })
+  it('shows access denied message when user is not older than 18', () => {
+    const wrapper = shallowMount(PageAccess, {
+      propsData: {
+        date: new Date()
+      }
+    })
+    wrapper.find('button').trigger('click')
+    expect(wrapper.vm.$data.msg).toBe(notOlderThanEighteenMessage)
   })
 })
