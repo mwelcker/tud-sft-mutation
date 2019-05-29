47f9221ffe5f361c4432760fe4a03a1916dd1fba
Alexander Lichter <manniL@gmx.net>
2019-05-29 15:55:04 +0200

    fix: invert check, omfg

--- src/components/PageAccess.vue
+++ src/components/PageAccess.vue
@@ -24,21 +24,21 @@ export default {
   },
   computed: {
     isOlderThanEighteen() {
-      return this.calculateAge(this.date)
+      return this.calculateAge(this.date) >= 18
     }
   },
   methods: {
     calculateAge(birthdayDate) {
       const ageDifferenceInMilliseconds = Date.now() - birthdayDate.getTime()
       const ageDate = new Date(ageDifferenceInMilliseconds)
-      return Math.abs(ageDate.getUTCFullYear() - 1970) >= 18
+      return Math.abs(ageDate.getUTCFullYear() - 1970)
     },
     verify() {
       if (!this.date) {
         this.msg = dateNotSetMessage
         return
       }
-      if (this.isOlderThanEighteen) {
+      if (!this.isOlderThanEighteen) {
         this.msg = notOlderThanEighteenMessage
         return
       }
