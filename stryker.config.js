01afdfef92e220fae8741b8dc45faa2db019f74b
Alexander Lichter <manniL@gmx.net>
2019-05-29 16:23:33 +0200

    chore: add jest config link

--- stryker.config.js
+++ stryker.config.js
@@ -3,6 +3,9 @@ module.exports = function (config) {
     mutate: ['src/components/PageAccess.vue', '!src/main.js', '!src/constants.js'],
     mutator: 'vue',
     testRunner: 'jest',
+    jest: {
+      config: require('./jest.config.js')
+    },
     reporter: ['progress', 'clear-text', 'html'],
     coverageAnalysis: 'off'
   })
