64d7b6c96a03856783ba92b76437fb2e920373f4
Martin Welcker <Martinwelcker@gmx.de>
2019-07-10 12:12:56 +0200

    Fix platform based issues with environment variables in stryker command  + Add html reporter

--- stryker.config.js
+++ stryker.config.js
@@ -2,6 +2,10 @@ module.exports = function (config) {
   config.set({
     mutate: ['src/components/PageAccess.vue', '!src/main.js', '!src/constants.js'],
     mutator: 'vue',
+    reporters: ['html'],
+    htmlReporter: {
+      baseDir: 'reports/mutation/html'
+    },
     testRunner: 'jest',
     reporter: ['progress', 'clear-text', 'html'],
     coverageAnalysis: 'off'
